583efa9744360948f2c6fb73a42d65330834d825
Pratham <prrrathm@gmail.com>
2021-09-07 19:32:54 +0530

    comments to lab assignment added

--- Sem3/Python/Lab/Lab1.py
+++ Sem3/Python/Lab/Lab1.py
@@ -1,16 +1,24 @@
+# RIGHT TRIANGLE FUNCTION
 def righttriangle(row) :
     for i in range(1, row+1) :
         print(i* '*')
 
+#INVERTED TRIANGLE FUNTION
 def invertedtriangle(row) :
     space = 0
     for i in range(row, 0, -1) :
         print( (space)*' ' + i*'*' + (i-1)*'*')
         space = space+1
 
+#MAIN FUNCTION
 def main() :
-    r = int(input('row : '))
-    choice = input('Enter right for right and invert for inverted triangle : ')
+    #INPUT NO. OF ROWS AND TYPE OF TRIANGLE
+    r = int(input('no. of rows : '))
+    choice = input(
+        '''Enter 'right' for right and 'inverted' for inverted triangle : '''
+        )
+
+    #TERNARY EQUATION
     righttriangle(r) if choice == 'right' else invertedtriangle(r)
 
 main()
