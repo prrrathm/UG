1c9e8937733e4804b98a8953f8240fe7fd5c15f5
Pratham Sharma <prrrathm@gmail.com>
2021-09-19 17:55:50 +0530

    fork 5 done

--- Sem3/OS/Assignment4Fork/fork5.cpp
+++ Sem3/OS/Assignment4Fork/fork5.cpp
@@ -1,8 +1,8 @@
-Exercises 153
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
 using namespace std;
+
 #define SIZE 5
 int nums[SIZE] = {0,1,2,3,4};
 
@@ -14,13 +14,13 @@ int main() {
     if (pid == 0) {
         for (i = 0; i < SIZE; i++) {
             nums[i] *= -i;
-            printf("CHILD: %d ",nums[i]); /* LINE X */
+            printf("\nCHILD: %d ",nums[i]); /* LINE X */
         }
     }
     else if (pid > 0) {
         // wait(NULL);
         for (i = 0; i < SIZE; i++)
-            printf("PARENT: %d ",nums[i]); /* LINE Y */
+            printf("\nPARENT: %d ",nums[i]); /* LINE Y */
     }
     return 0;
 }
