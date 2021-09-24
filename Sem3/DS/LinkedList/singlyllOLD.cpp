eb1be94719602d8c5605cab60896816ee487cf1c
Pratham Sharma <prrrathm@gmail.com>
2021-09-24 11:38:20 +0530

    new singly created

---
+++ Sem3/DS/LinkedList/singlyllOLD.cpp
@@ -0,0 +1,228 @@
+#include <iostream>
+#include <random>
+#include <time.h>
+using namespace std;
+
+class node{
+    public :
+        int data;
+        node* next;
+        node(int value){
+            data = value;
+        }
+};
+
+void displayll(node* head);
+void insertAfter(node* &head, int value);
+void insertBefore(node* &head, int value);
+void insertRandom(node* &head, int value);
+void deleteBegin(node* &head);
+void deleteEnd(node* &head);
+void deletell(node* &head, int key);
+bool searchll(node* head, int key);
+
+int main(){
+
+    node* head;
+    insertAfter(head,0);
+    int choice, value, pos;
+    char flag;
+
+    cout << "List of Operations\n";
+    cout << "1. Insert in Beginning\n";
+    cout << "2. Insert at Last\n";
+    cout << "3. Insert at Random Location\n";
+    cout << "4. Delete from Beginning\n";
+    cout << "5. Delete from Last\n";
+    cout << "6. Delete node after given location\n";
+    cout << "7. Search for an element\n";
+
+    while(true){
+        cout << "Enter serial number of operation to perform : ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1 : {
+            cout << "Enter value : ";
+            cin >> value;
+            insertBefore(head,value);
+            displayll(head);
+            break;
+        }
+        case 2 : {
+            cout << "Enter value : ";
+            cin >> value;
+            insertAfter(head,value);
+            displayll(head);
+            break;
+        }
+        case 3 : {
+            cout << "Enter value : ";
+            cin >> value;
+            insertRandom(head,value);
+            displayll(head);
+            break;
+        }
+        case 4 : {
+            deleteBegin(head);
+            displayll(head);
+            break;
+        }
+        case 5 : {
+            deleteEnd(head);
+            displayll(head);
+            break;
+        }
+        case 6 : {
+            cout << "Enter location : ";
+            cin >> value;
+            value++;
+            deletell(head,value);
+            displayll(head);
+            break;
+        }
+        case 7 : {
+            cout << "Enter element to search : ";
+            cin >> value;
+            // displayll(head);
+            if(searchll(head,value)){
+                cout << "yes element is present\n";
+            }
+            else{
+                cout << "no element is not present\n";
+            }
+            break;
+        }
+        default:{
+            cout << "Invalid Input.\n";
+            break;
+        }
+        }
+        cout << "\nDo you want to continue?(y/n) : ";
+        cin >> flag;
+        if(flag == 'n' || flag == 'N'){
+            break;
+        }
+    }
+
+    return 0;
+}
+
+void displayll(node* head){
+    while(head != NULL){
+        cout << head->data << "->";
+        head = head->next;
+    }
+    cout << "NULL" << endl;
+}
+
+void insertAfter(node* &head, int value){
+    node* n = new node(value);
+    if(head == NULL){
+        head = n;
+        return;
+    }
+    node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void insertBefore(node* &head, int value){
+    if(head == NULL){
+        head = new node(value);
+        return;
+    }
+    node* temp = head;
+    head = new node(value);
+    head->next = temp;
+}
+
+void insertRandom(node* &head, int value){
+    if (head == NULL){
+        head = new node(value);
+        return;
+    }
+    srand(time(0));
+    int size= 0;
+    node* temp1 = head;
+    while(temp1 != NULL){
+        size++;
+        temp1 = temp1->next;
+    }
+    
+    int i = (rand() % size);
+    
+    temp1 = head;
+    while(i--){
+        temp1 = temp1->next;
+    }
+    
+    node* temp2 = temp1->next;
+    temp1->next = new node(value);
+    temp1->next->next = temp2;
+}
+
+void deleteBegin(node* &head){
+    if(head == NULL){
+        cout << "List Empty" << endl;
+        return;
+    }
+    node* temp = head;
+    head = head->next;
+    delete temp;
+}
+
+void deleteEnd(node* &head){
+    if (head == NULL){
+        cout << "List Empty" ;
+        return;
+    }
+    node* temp = head;
+    if (temp->next == NULL){
+        head = NULL;
+        delete temp;
+        return;
+    }
+    while(temp->next->next != NULL){
+        temp = temp->next;
+    }
+    node* temp2 = temp->next;
+    temp->next = NULL;
+    delete temp2;
+}
+
+void deletell(node* &head, int key){
+    if(head == NULL)
+        return;
+    
+    node* temp = head;
+    if (temp->next == NULL){
+        head = NULL;
+        delete temp;
+        return;
+    }
+    key--;
+    while(--key){
+        temp = temp->next;
+    }
+    node* temp2 = temp->next;
+    temp->next = temp->next->next;
+    delete temp2;
+
+}
+
+bool searchll(node* head, int key){
+    if(head == NULL)  {  
+        printf("\nEmpty List\n");  
+    }  
+    node* temp = head;
+    while(temp != NULL){
+        if(key == temp->data){
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
