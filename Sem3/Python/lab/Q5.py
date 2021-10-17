e948f7fc6924aa41dc129397601a285baeb4a08c
Pratham Sharma <prrrathm@gmail.com>
2021-10-17 16:59:09 +0530

    Q5 correction

--- Sem3/Python/lab/Q5.py
+++ Sem3/Python/lab/Q5.py
@@ -1,15 +1,10 @@
 # pattern - 1–x2/2!+x4/4!–x6/6!+...xn/n!
-
-def factorial(num) :
-    if num == 1:
-        return num
-    else :
-        return num * factorial(num-1)
+from Q4 import Factorial
 
 def patternsum(x,n) :
     patternsum = 1
     for i in range(2, n+1) :
-        patternsum = patternsum + (-1**(i-1)) * i*x / factorial(i)
+        patternsum = patternsum + (-1**(i-1)) * i*x / Factorial(i)
     return patternsum
 
 n = int(input())
