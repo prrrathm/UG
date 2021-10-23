9b7e464571c210e0ba45d5e219ad3358194c863b
Pratham Sharma <prrrathm@gmail.com>
2021-10-23 11:24:28 +0530

    Update Q9.py

--- Sem3/Python/lab/Q9.py
+++ Sem3/Python/lab/Q9.py
@@ -2,12 +2,23 @@ import os
 os.system('clear')
 
 StudentScores = dict({})
-StudentScores.get('')
+StudentScores = {
+    'Shaun': {'DS': 50, 'OS': 35, 'CN': 45, 'Python':30, 'GE':50},
+    'Ritika': {'DS': 46, 'OS': 31, 'CN': 30, 'Python':30, 'GE':50},
+    'Smriti': {'DS': 46, 'OS': 31, 'CN': 30, 'Python':30, 'GE':50},
+    'Jacob': {'DS': 50, 'OS': 50, 'CN': 50, 'Python':30, 'GE':50},
+}
 
-def TopStudent(studDict) :
-    for i, v in studDict :
-        print(i,v)
-    return
+def TopStudent(stud) :
+    lis = [0]
+    try :
+        for k,v in stud.items() :
+            # S = 
+            lis.append( sum(v.values()) )
+    except : TypeError and ValueError
+    pos = lis.index(max(lis)) - 1
+    print( max(lis), list(stud)[pos])
+    
 
 while(True) :
     print(
@@ -17,9 +28,9 @@ while(True) :
         '3. View Top Student\n',
         '4. Exit'
     )
-    option = int(input('->'))
+    option = input('->')
         
-    if option == 1 : 
+    if option == '1' : 
         try :
             name = input('Enter Name of Student -> ')
             print('Enter Marks in Subjects')
@@ -28,7 +39,9 @@ while(True) :
             CN = float(input('CN -> '))
             Py = float(input('Python ->'))
             GE = float(input('GE -> '))
-                
+            
+            assert DS < 50 and OS < 50 and CN < 50 and Py < 50 and GE < 50, 'Marks out of Bound'
+            
             StudentScores[name] = {
                 'DS':DS,
                 'OS':OS,
@@ -38,15 +51,16 @@ while(True) :
             }
         except : ValueError or NameError        
     
-    elif option == 2 : 
+    elif option == '2' : 
         print(StudentScores)
         
-    elif option == 3 :
+    elif option == '3' :
         TopStudent(StudentScores)
         
-    elif option == 4 :
+    elif option == '4' :
         exit()
-
+    else :
+        continue
 
 # 9. Use dictionary to store marks of the students in 4 subjects.
 # Write a function to find the name of the student securing highest percentage.
