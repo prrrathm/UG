c1ee2045cbb88553be6d891ad298c1a3793813e5
Pratham Sharma <prrrathm@gmail.com>
2021-11-26 00:00:38 +0530

    exponent and menu driven function added

--- Sem3/Python/lab/Q13.py
+++ Sem3/Python/lab/Q13.py
@@ -6,22 +6,61 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import os
 
 # Sine and Cosine
-x = list()
-y = list()
-z = list()
+os.system("clear")
+while(True) :
+    print("Select your Graph\n",
+        "1. Sine Function\n",
+        "2. Cosine Function\n",
+        "3. Polynomial Function\n",
+        "4. Exponential Function\n",
+        "5. Exit")
 
-for i in range(0,200) :
-    i = i/10
-    x.append(i)
-    y.append(math.sin(i))
-    z.append(math.cos(i))
+    choice = int(input("=>"))
 
-plt.plot(x,y,x,z)
-plt.xlabel('x -> from 0 to 4pi')
-plt.ylabel('sin(x) and cos(x)')
-plt.title('Plot of sin and cos from 0 to 4pi')
-plt.legend(['sin(x)', 'cos(x)'])
-plt.show()
+    if choice == 1 :
+        x = list()
+        y = list()
+        for i in range(0,200) :
+            i = i/10
+            x.append(i)
+            y.append(math.sin(i))
+        plt.plot(x,y)
+        plt.xlabel('x -> from 0 to 4pi')
+        plt.ylabel('sin(x)')
+        plt.title('Plot of sin from 0 to 4pi')
+        plt.legend(['sin(x)'])
+        plt.show()
 
+    elif choice == 2 :
+        x = list()
+        y = list()
+        for i in range(0,200) :
+            i = i/10
+            x.append(i)
+            y.append(math.cos(i))
+        plt.plot(x,y)
+        plt.xlabel('x -> from 0 to 4pi')
+        plt.ylabel('cos(x)')
+        plt.title('Plot of cos from 0 to 4pi')
+        plt.legend(['cos(x)'])
+        plt.show()
+    
+    elif choice == 4 :
+        e = int(input("Enter exponent =>"))
+        x = list()
+        y = list()
+        for i in range(0,50) :
+            x.append(i)
+            y.append(i**e)
+        plt.plot(x,y)
+        plt.xlabel('x -> 50')
+        plt.ylabel('x^'+str(e))
+        plt.title('Plot of exponent '+str(e))
+        plt.legend(['x^'+str(e)])
+        plt.show()
+        
+    elif choice == 5 :
+        exit()
